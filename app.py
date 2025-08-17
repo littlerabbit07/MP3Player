3f5190bf61686f42853354f9a3b08995b34c8bad
littlerabbit07 <49513403+littlerabbit07@users.noreply.github.com>
2025-08-17 00:39:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,24 @@ import random
 app = Flask(__name__)
 
 # Set this to the full path of your external music folder
-MUSIC_FOLDER = "D:/Music"  # Change this to your folder path
+MUSIC_FOLDER = "/home/natalia/Music"  # Change this to your folder path
 
 @app.route('/')
 def index():
+    tracks = []
     if os.path.isdir(MUSIC_FOLDER):
-        tracks = [f for f in os.listdir(MUSIC_FOLDER) if f.endswith('.mp3')]
+        for root, dirs, files in os.walk(MUSIC_FOLDER):
+            for f in files:
+                if f.endswith('.mp3'):
+                    # Store relative path from MUSIC_FOLDER for serving
+                    rel_dir = os.path.relpath(root, MUSIC_FOLDER)
+                    rel_file = os.path.join(rel_dir, f) if rel_dir != '.' else f
+                    tracks.append(rel_file)
         random.shuffle(tracks)
-    else:
-        tracks = []
     return render_template('index.html', tracks=tracks)
 
 # Route to serve the MP3 files from external folder
-@app.route('/music/<filename>')
+@app.route('/music/<path:filename>')
 def music(filename):
     return send_from_directory(MUSIC_FOLDER, filename)
 
